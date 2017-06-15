453fb96e263b83039c9c69664e239591af9c1296
peopleyou <373003713@qq.com>
2017-06-15 12:25:25 +0800

    使用spring cloud实现分布式配置管理

---
+++ cloud-config-server/src/main/java/com/study/cloud/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.study.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by yuanyouz on 2017/6/15.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
